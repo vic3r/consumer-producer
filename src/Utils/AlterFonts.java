ba1b17a09b397224d1a9ff8a89288ba695b5d377
vic3r <barcelonamane@hotmail.com>
2018-04-08 01:18:54 -0500

    Add bootstrap graphics

---
+++ src/Utils/AlterFonts.java
@@ -0,0 +1,19 @@
+package Utils;
+
+
+import java.awt.Font;
+
+import javax.swing.UIManager;
+
+public class AlterFonts {
+	public static void alterFonts(){
+		UIManager.put("Menu.font", new Font("SansSerif", Font.TRUETYPE_FONT, 12));
+		UIManager.put("MenuItem.font", new Font("SansSerif", Font.TRUETYPE_FONT, 12));
+		UIManager.put("Label.font", new Font("SansSerif", Font.TRUETYPE_FONT, 12));
+		UIManager.put("Button.font", new Font("SansSerif", Font.TRUETYPE_FONT, 12));
+		UIManager.put("RadioButton.font", new Font("SansSerif", Font.TRUETYPE_FONT, 12));
+		UIManager.put("CheckBox.font", new Font("SansSerif", Font.TRUETYPE_FONT, 12));
+		UIManager.put("TitledBorder.font", new Font("SansSerif", Font.TRUETYPE_FONT, 12));
+		UIManager.put("ComboBox.font", new Font("SansSerif", Font.TRUETYPE_FONT, 12));
+	}
+}
